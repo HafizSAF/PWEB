124ffec07df300743753831e66e250ecfb405d02
Tweeeez <hafizsyafiq936@gmail.com>
2025-04-24 05:00:23 +0700

    update 1

--- aksara.js
+++ aksara.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const homeLink = document.getElementById("aksara-link");
+    const aksaraLink = document.getElementById("aksara-link");
 
     // Mendapatkan URL saat ini
     const currentUrl = window.location.href;
 
     // Memeriksa apakah pengguna berada di halaman utama
     if (currentUrl.includes("aksara.html")) {
-        homeLink.classList.add("active");
+        aksaraLink.classList.add("active");
     }
 });
